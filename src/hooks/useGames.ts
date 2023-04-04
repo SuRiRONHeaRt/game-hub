7085255f27f7451a8fd16c31bed2d6109989ebd8
raveen <raveenranadewa@gmail.com>
2023-04-04 16:29:23 +0530

    Displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,10 +3,20 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  //? මේක මෙහෙම උනේ RAWG.IO එක design කරල තියෙන විදියෙ අවුලක් හින්ද,
+  //* this is an array of objects, where each object has a property called "platform" of
+  //* type Platform.
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
