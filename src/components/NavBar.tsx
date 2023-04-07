df67fe9e39a6754d323bf7c94c524a7a25eb99a8
raveen <raveenranadewa@gmail.com>
2023-04-07 12:34:39 +0530

    Build search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,15 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/GameHub Resources/Logo/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
     /* navbar එක  horizontal stack එකක් විදියට.. */
     /* works just like a flex-container */
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
